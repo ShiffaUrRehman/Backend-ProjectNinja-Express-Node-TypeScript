dccb67abae92093cff90ab24d81c5d0cb27ff077
Shiffa Ur Rehman <22100167@lums.edu.pk>
2023-09-22 17:43:59 +0500

    Saving hashed passwords

--- src/user/user_model.ts
+++ src/user/user_model.ts
@@ -1,8 +1,10 @@
 import * as mongoose from 'mongoose';
 import {User, Role} from './user_interface';
 import { NextFunction } from 'express';
+import * as bcrypt from "bcrypt";
 
 
+const SALT_WORK_FACTOR = 10;
 
 const userSchema = new mongoose.Schema(
   {
@@ -31,26 +33,26 @@ const userSchema = new mongoose.Schema(
   },
 );
 
-// userSchema.pre("save", function (next: NextFunction) {
-//     let user: User & mongoose.Document = this;
+userSchema.pre("save", function (next: NextFunction) {
+    let user = this; // what would be the type here
   
-//     // only hash the password if it has been modified (or is new)
-//     if (!user.isModified("password")) return next();
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified("password")) return next();
   
-//     // generate a salt
-//     bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-//       if (err) return next(err);
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+      if (err) return next(err);
   
-//       // hash the password using our new salt
-//       bcrypt.hash(user.password, salt, function (err, hash) {
-//         if (err) return next(err);
+      // hash the password using our new salt
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
   
-//         // override the cleartext password with the hashed one
-//         user.password = hash;
-//         next();
-//       });
-//     });
-//   });
+        // override the cleartext password with the hashed one
+        user.password = hash;
+        next();
+      });
+    });
+  });
 
 const userModel = mongoose.model<User & mongoose.Document>('User', userSchema);
 
